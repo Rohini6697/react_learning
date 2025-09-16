9165483e3ccb99484b8a5c4866060b763d420290
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-09-16 20:05:06 +0530

    another commit

--- components/components-app/src/components/Home.jsx
+++ components/components-app/src/components/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
 const Home = (props) => {
+  const list = [
+    {id : 1,
+      name : 'apple'
+    },
+    {
+      id : 2,
+      name : 'mango'
+    },
+    {
+      id : 3,
+      name : 'banana'
+    }
+  ]
   return (
     <div>
         <h1>hello,{props.username}</h1>
+        <ul>
+          {
+            list.map((fruits) => (
+              <li key={fruits.id}>{fruits.name}</li>
+            ))
+          }
+        </ul>
       
     </div>
   )
